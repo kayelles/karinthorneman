a95fdaa6908141513ab1922a7fb76ecf7e782660
Ludvig Sundstrom <lud.sund@gmail.com>
2014-10-12 15:45:29 +0200

    working the crossfader and some logos

--- galleri/script.js
+++ galleri/script.js
@@ -6,7 +6,7 @@
 *                                                                            *
 *	[ ] Fix the front page 													 * 
 *	[ ] Fix the bug with remove 											 * 
-*	[x] make images able to pop out 										 * 
+*	[ ] make images able to pop out 										 * 
 *	[ ] fix gallery page (limit clicks, make pop out image bigger)           *
 *	[ ] Add error checking for file and user input							 * 
 *	[ ] Add functionality for updating description and text fields  		 * 
@@ -61,22 +61,60 @@ $(document).ready(function() {
             render();
             
             // Handle events
-
-            $('.clickable').on('click', function(e) { 
-                handleClick($(this), e)
-            });
+			$('.clickable').on('click', function(e) { 
+				handleclick($(this), e)
+			});
             $(document).on("keydown", function(e) { handleKeyPress(e) });
+
+
 
         },
         "error" : function() { alert("Error: Content could not be loaded"); }
     });
 });
 
+/*
+    Appends images to the webpage
+*/
+
+function getImagedata() {
+    imageCount  = 0;
+    imageList   = [];
+
+    for (i = 0; i < imageData["paths"].length; i++) {
+        if (imageData["exhibitions"][i] == exhibition) {
+			var imagetag = '<img data-lightbox="image1" src="' + 
+							imageData["paths"][i] + '" />';
+			var imageLink = '<a href="' + imageData["paths"][i] + 
+							'" data-lightbox="image1">' + imagetag + '</a>';
+            imageCount++;
+            imageList.push(imageLink);
+        }
+    }
+}
+
+function addImages() {
+	for (i = 0; i < imageList.length; i++) {
+		$("#crossfade").append(imageList[i]);
+	}
+	$("#crossfade a:first-child").addClass("opaque");
+}
+
+function showImage() {
+	$("#crossfade a").removeClass("opaque");
+	$("#crossfade a").css('z-index', 0);
+	var newImage = $("#crossfade a:nth-child(" + (index + 1) + ")");
+	newImage.css('z-index', 1);
+	newImage.addClass("opaque");
+	updateSidebar();
+}
+
 
 function render() {
     getImagedata();
-    showImage(index);
-    renderSidebar(index);
+	addImages();
+	showImage();
+    updateSidebar();
     screenheight = screen.height;
     if ($(window).height() > 1000) {
         $("#container").css("height", $(window).height());
@@ -86,35 +124,50 @@ function render() {
     }
 }
 
+/* Renders the container next to the image and its contents */
+
+function updateSidebar() {
+    $("#control").css("height", imageData["heights"][index]);
+    var desc        = $("#description");
+    var whichimage  = $("#whichimage");
+    desc.empty();
+    desc.append(imageData["descs"][index]);
+    whichimage.empty();
+    whichimage.append((index + 1) + "/" + imageCount);
+}
+
 /*
     Handles the event when an element is clicked 
 */
 
-function handleClick(caller, e) {
-    if (caller.hasClass('octicon-chevron-left')) {
-        index--;
-        if (index < 0) {
-            index = imageCount - 1;
-        }
-        showImage(index);
-    }
-    else if (caller.hasClass('octicon-chevron-right')) {
-        index++;
-        if (index == imageCount) {
-            index = 0;
-        }
-        showImage(index);
-    }
-    else if (e.target.id == 1) {
-        exhibition = "2014";
-        index = 0;
-        render();
-    }
-    else if (e.target.id == 2) {
-        exhibition = "2010";
-        index = 0;
-        render();
-    }
+function handleclick(caller, e) {
+	if (caller.hasClass('octicon-chevron-left')) {
+		index--;
+		if (index < 0) {
+			index = imageCount - 1;
+		}
+		showImage();
+	}
+	else if (caller.hasClass('octicon-chevron-right')) {
+		index++;
+		if (index == imageCount) {
+			index = 0;
+		}
+		showImage();
+	}
+	else if (e.target.id == 1) {
+		exhibition = "2014";
+		index = 0;
+		render();
+	}
+	else if (e.target.id == 2) {
+		exhibition = "2010";
+		index = 0;
+		render();
+	}
+	else if (caller.is($("#crossfade a"))) {
+		//something else 
+	}
 }
 
 /*
@@ -127,87 +180,14 @@ function handleKeyPress(e) {
         if (index < 0) {
             index = imageCount - 1;
         }
-        showImage(index);
+        showImage();
     }
     else if (e.which == 39) {
         index++;
         if (index == imageCount) {
             index = 0;
         }
-        showImage(index);
+        showImage();
     }
-}
-
-
-/*
-    Appends images to the webpage
-*/
-
-function getImagedata() {
-    imageCount  = 0;
-    imageList   = [];
-
-    for (i = 0; i < imageData["paths"].length; i++) {
-        if (imageData["exhibitions"][i] == exhibition) {
-            var imagetag = '<img src="' + imageData["paths"][i] + '" />';
-            var anchor = '<a href="' + imageData["paths"][i] + '" data-lightbox="image-' + i
-                +  '">' + imagetag + '</a>';
-            imageCount++;
-            imageList.push(anchor);
-        }
-    }
-}
-/* Displays an image with index index  */
-
-function showImage(index) {
-    var image = imageList[index];
-    var imagetag = $("#image");
-    var imagetag2 = $("#image2");
-    var delay = 1000;
-    imagetag2.css({opacity : 0});
-    imagetag2.empty();
-    imagetag2.append(image);
-    imagetag2.animate({opacity : 1}, delay);
-    imagetag.animate({opacity : 0}, delay, function() {
-            imagetag.empty();
-            imagetag.append(image)
-        }).animate({opacity : 1}, delay);
-    renderSidebar();
-}
-
-
-/* Hides an image with index index  */
-
-
-/* Renders the container next to the image and its contents */
-
-function renderSidebar() {
-    $("#control").css("height", imageData["heights"][index]);
-    var desc        = $("#description");
-    var whichimage  = $("#whichimage");
-    desc.empty();
-    desc.append(imageData["descs"][index]);
-    whichimage.empty();
-    whichimage.append((index + 1) + "/" + imageCount);
-}
-
-/*
-    Attempts to fade out elements that is a member of class "fadeout"
-*/
-
-
-/*
-    Shows faded out elements
-*/
-
-function showFaded() {
-    $('.fadeout').css('opacity' , 1); 
-    $('.mega-octicon').css('opacity', 0.2);
-    $('.mega-octicon').mouseenter(function() {
-        $(this).css('opacity', 1);
-    });
-    $('.mega-octicon').mouseleave(function() {
-        $(this).css('opacity', 0.2);
-    });
 }
 
